cf75fec5ee8228b95a01a176911333610b136328
Larissa Moreira <larissamgusmao@gmail.com>
2018-09-04 19:33:17 -0300

    login/signup

--- umas_e_ostras/settings.py
+++ umas_e_ostras/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reservas',
-    'bloguinho'
+    'bloguinho',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- umas_e_ostras/urls.py
+++ umas_e_ostras/urls.py
@@ -5,5 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reservas/', include(('reservas.urls', 'reservas'), namespace='reservas')),
-    path('bloguinho/', include('bloguinho.urls'))
+    path('bloguinho/', include('bloguinho.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
